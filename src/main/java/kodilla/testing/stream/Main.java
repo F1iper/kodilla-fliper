5bde8d055a262b34645bcef7bb234e74d356cb83
Filip <ftimofiejew@gmail.com>
2020-04-06 23:18:32 +0200

    Stream first touch

--- src/main/java/kodilla/testing/stream/Main.java
+++ src/main/java/kodilla/testing/stream/Main.java
@@ -1,16 +1,42 @@
 package kodilla.testing.stream;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
     public static void main(String[] args) {
 
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        People.getList()
+                .stream()
+                .filter(s -> s.length() >11)
+                .map(s -> s.toUpperCase())
+                .forEach(s -> System.out.println(s));
 
-        poemBeautifier.beautify("This is going to be beautifull", (s) -> System.out.println(s.toUpperCase()));
-        poemBeautifier.beautify("This is sentence number two", (s) -> System.out.println("*** " + s + " ***"));
-        poemBeautifier.beautify("An0tH3r type 0f s3nt3nc3",
-                (s) -> System.out.println(s.replace("0", "o").replace("3", "e")));
-        poemBeautifier.beautify("sentence number four",
-                (s) -> System.out.println(s.substring(8, 16).toUpperCase().concat("") + s + s.substring(8, 16).toUpperCase().concat("")));
+//        People.getList()                       //wywolanie metody statycznej
+//                .stream()                      //wywolanie strumienia
+//                .map((s) -> s.toUpperCase())   //wywolanie metody map(Function mapper) przyjmuje ona wyrazenie lambda
+//                .forEach(System.out::println); // for each to operacja terminalna
 
+        People.getNumbers()                     //wywolanie metody statycznej getNumbers znajdujacej sie w klasie People
+                .stream()                       //wywolanie strumienia
+                .mapToDouble(i -> i += i *i)    //operacja na strumieniu, tworzony zostaje nowy obiekt
+                .filter(i -> i > 5)
+                .forEach(System.out::println);  //wywolanie metody forEach, czyli wyswietlenie kazdego obiektu ze zwracanej listy
+
+        People.getList()
+                .stream()
+                .map(String::toUpperCase)
+                .forEach(System.out::println);  //METHOD REFERENCE, zamiennie z wyrazeniem lambda
+
+
+//        PoemBeautifier poemBeautifier = new PoemBeautifier();
+//
+//        poemBeautifier.beautify("This is going to be beautifull", (s) -> System.out.println(s.toUpperCase()));
+//        poemBeautifier.beautify("This is sentence number two", (s) -> System.out.println("*** " + s + " ***"));
+//        poemBeautifier.beautify("An0tH3r type 0f s3nt3nc3",
+//                (s) -> System.out.println(s.replace("0", "o").replace("3", "e")));
+//        poemBeautifier.beautify("sentence number four",
+//                (s) -> System.out.println(s.substring(8, 16).toUpperCase().concat("") + s + s.substring(8, 16).toUpperCase().concat("")));
+//
+//
     }
 }
--- src/main/java/kodilla/testing/stream/People.java
+++ src/main/java/kodilla/testing/stream/People.java
@@ -5,18 +5,27 @@ import java.util.List;
 
 public final class People {
 
-    public static List<String> getList(){
+    public static List<String> getList() {
         final List<String> theList = new ArrayList<>();
-        theList.add("John Doe");
-        theList.add("Jessica Simpson");
-        theList.add("John Rambo");
-        theList.add("Hannibal");
-        return new ArrayList<> (theList);
+        theList.add("John Smith");
+        theList.add("Dorothy Newman");
+        theList.add("John Wolkowitz");
+        theList.add("Lucy Riley");
+        theList.add("Owen Rogers");
+        theList.add("Matilda Davies");
+        theList.add("Declan Booth");
+        theList.add("Corinne Foster");
+        theList.add("Khloe fry");
+        theList.add("Martin Valenzuela");
+        return new ArrayList<>(theList);
     }
 
-    public static void main(String[] args) {
-
-
+        public static List<Integer> getNumbers(){
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        return new ArrayList<>(numbers);
     }
-
 }
